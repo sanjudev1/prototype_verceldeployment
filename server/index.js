d3de46272610362dafbee2948156d89cb7bac046
sanjeev ch <112742845+sanjudev1@users.noreply.github.com>
2024-07-28 23:28:17 +0530

    suggestions api added

--- server/index.js
+++ server/index.js
@@ -5,7 +5,18 @@ const app = express();
 const port = 3010;
 
 app.use(cors());
+app.use((req, res, next) => {
+    const corsWhitelist = [
+        'https://youtube-app-sanju-vert.vercel.app',
+        'https://prototype-verceldeployment-client.vercel.app',
+    ];
+    if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
 
+    next();
+});
 // Static data to simulate database content
 const users = [
   { name: 'John Doe', age: 30 },
@@ -25,6 +36,14 @@ app.get('/get', (req, res) => {
   const data = users.filter(user => user.age > 20);
   res.status(200).json(data);
 });
+
+app.get('/search',async(req,res)=>{
+  const search = req.query.q
+  const data = await fetch('https://suggestqueries.google.com/complete/search?client=firefox&q='+search)
+  const response = await data.json()
+  res.status(200).json(response);
+}
+
 
 // Start the server
 app.listen(port, () => {
